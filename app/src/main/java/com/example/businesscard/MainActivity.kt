44496afe4bd49987b14ea4b04459debee20b7a5c
AndroidAlex2024 <a.a.demenev.market@gmail.com>
2024-11-12 21:36:42 +0500

    update

--- app/src/main/java/com/example/businesscard/MainActivity.kt
+++ app/src/main/java/com/example/businesscard/MainActivity.kt
@@ -7,8 +7,12 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.absolutePadding
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -44,18 +48,29 @@ class MainActivity : ComponentActivity() {
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Column(
 //horizontalAlignment = Alignment.CenterHorizontally,
-
-        Modifier.fillMaxSize()
+        horizontalAlignment = Alignment.CenterHorizontally,
+        //verticalArrangement = Arrangement.Center,
+        modifier = modifier.fillMaxSize()
         //Modifier
-        .padding(10.dp)
+            .padding(top=60.dp,start=20.dp,end=20.dp,bottom=20.dp)
            // .background(Color.Blue)
     ) {
-        Column {
-val image = painterResource(R.drawable.android_logo)
+        Column (
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = modifier
+                .padding(top=0.dp,start=0.dp,end=0.dp,bottom=0.dp)
+                .background(Color.Red)
+               // .fillMaxHeight()
+        ){
+
+val         image = painterResource(R.drawable.android_logo)
             Image(
                 painter = image,
-                contentDescription = null
-
+                contentDescription = null,
+                        Modifier
+                            .padding(start=80.dp,end=80.dp)
+                            .background(Color.Blue)
             )
             Text(
                 text = "AndroidAlex2024",
@@ -71,7 +86,15 @@ val image = painterResource(R.drawable.android_logo)
                 modifier = modifier
             )
         }
-        Column {
+        Column (
+            verticalArrangement = Arrangement.Bottom,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = modifier
+                //.padding(top=120.dp,start=0.dp,end=0.dp,bottom=0.dp)
+                .background(Color.Red)
+                .fillMaxWidth()
+
+        ){
 
             Text(
                 text = "+7 950 000 00 00",
