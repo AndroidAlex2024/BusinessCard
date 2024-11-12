23c28836d50aa69e25ea61a8c5cc8d54d0487e58
AndroidAlex2024 <a.a.demenev.market@gmail.com>
2024-11-12 19:21:28 +0500

    update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.businesscard"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.businesscard"
